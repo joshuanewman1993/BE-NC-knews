081077efcc8de41374b0facf30802702d9225e9c
Joshua Newman <joshuanewman1993@gmail.com>
2019-01-21 09:01:10 +0000

    cors installed

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { handle400, handle404, handle422 } = require('./errors/index');
-
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const apiRouter = require('./routes/apiRoute');
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use('/api', apiRouter);
 
